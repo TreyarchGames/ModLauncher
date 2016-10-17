ad640b16339f99b045c260dca1ac9e4a201117b2
SE2Dev <brokensilencedev@gmail.com>
2016-10-17 19:44:49 -0400

    Add USE_CUSTOM_QT Definition
    Automatically Determine Which Qt Libraries to Link

--- stdafx.cpp
+++ stdafx.cpp
@@ -17,3 +17,21 @@
 */
 
 #include "stdafx.h"
+
+#if USE_CUSTOM_QT // If a custom version of Qt (e.g. Treyarch's version) is being used, use the custom library names
+
+#if _DEBUG
+#pragma comment(lib, "Qt5Core64d.lib")
+#pragma comment(lib, "Qt5Gui64d.lib")
+#pragma comment(lib, "Qt5Widgets64d.lib")
+#else
+#pragma comment(lib, "Qt5Core64r.lib")
+#pragma comment(lib, "Qt5Gui64r.lib")
+#pragma comment(lib, "Qt5Widgets64r.lib")
+#endif
+
+#else //If a standard version of Qt is being used, link the standard library names
+#pragma comment(lib, "Qt5Core.lib")
+#pragma comment(lib, "Qt5Gui.lib")
+#pragma comment(lib, "Qt5Widgets.lib")
+#endif
