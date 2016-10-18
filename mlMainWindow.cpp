3ca9d3c17531605a7f24f8f66fe3cf5b33fe8c4b
SE2Dev <brokensilencedev@gmail.com>
2016-10-17 21:33:36 -0400

    Modify Context Menu Order
    Moved Radiant to the second entry to make the menu more consistent with
    the Explorer context menu.

--- mlMainWindow.cpp
+++ mlMainWindow.cpp
@@ -379,15 +379,16 @@ void mlMainWindow::ContextMenuRequested(const QPoint& Point)
 
 	QMenu* Menu = new QMenu;
 	Menu->addAction(GameIcon, QString("Run %1").arg(ItemType), this, SLOT(OnRunMapOrMod()));
-	Menu->addAction("Edit Zone File", this, SLOT(OnOpenZoneFile()));
-	//Menu->addAction("Show Zone Source Folder", this, SLOT(OnOpenZoneFolder())); // is this really needed? Accessing root is better than zone_source itself
-	Menu->addAction(QString("Open %1 Folder").arg(ItemType), this, SLOT(OnOpenModRootFolder()));
 
 	if (Item->data(0, Qt::UserRole).toInt() == ML_ITEM_MAP)
 	{
 		QIcon RadiantIcon(":/resources/Radiant.png");
 		Menu->addAction(RadiantIcon, "Open Map in Radiant", this, SLOT(OnFileLevelEditor()));
 	}
+
+	Menu->addAction("Edit Zone File", this, SLOT(OnOpenZoneFile()));
+	//Menu->addAction("Show Zone Source Folder", this, SLOT(OnOpenZoneFolder())); // is this really needed? Accessing root is better than zone_source itself
+	Menu->addAction(QString("Open %1 Folder").arg(ItemType), this, SLOT(OnOpenModRootFolder()));
 
 	Menu->addSeparator();
 	Menu->addAction("Delete", this, SLOT(OnDelete()));
